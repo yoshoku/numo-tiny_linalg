9bfc47e3b5c556d2108a3d9767b3cf039cea2013
yoshoku <yoshoku@outlook.com>
2023-05-02 01:00:38 +0900

    chore: add auto-setting for BLAS and LAPACK libraries

--- ext/numo/tiny_linalg/extconf.rb
+++ ext/numo/tiny_linalg/extconf.rb
@@ -2,4 +2,21 @@
 
 require 'mkmf'
 
+use_accelerate = false
+if RUBY_PLATFORM.include?('darwin') && have_framework('Accelerate')
+  $CFLAGS << ' -DTINYLINALG_USE_ACCELERATE'
+  use_accelerate = true
+end
+
+unless use_accelerate
+  if have_library('openblas')
+    $CFLAGS << ' -DTINYLINALG_USE_OPENBLAS'
+  else
+    abort 'libblas is not found' unless have_library('blas')
+    $CFLAGS << ' -DTINYLINALG_USE_BLAS'
+  end
+
+  abort 'liblapack is not found' if !have_func('dsyevr') && !have_library('lapack')
+end
+
 create_makefile('numo/tiny_linalg/tiny_linalg')
