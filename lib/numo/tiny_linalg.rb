36f8a3cd7055ba7aa8cd182e10e5b5adffaaa4ed
yoshoku <yoshoku@outlook.com>
2023-05-04 15:23:43 +0900

    chore: add numo-narray loading

--- lib/numo/tiny_linalg.rb
+++ lib/numo/tiny_linalg.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'tiny_linalg/version'
-require_relative 'tiny_linalg/tiny_linalg'
+if defined?(Numo::Linalg)
+  warn 'Numo::Linalg is already defined. Numo::TinyLinalg will not be loaded.'
+else
+  require 'numo/narray'
+  require_relative 'tiny_linalg/version'
+  require_relative 'tiny_linalg/tiny_linalg'
 
-module Numo
-  module TinyLinalg
-    class Error < StandardError; end
-    # Your code goes here...
+  module Numo
+    module TinyLinalg
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+
+    # Linalg = TinyLinalg
   end
 end
