d265723c73d4f28db6ae673c732460257ebe83a4
yoshoku <yoshoku@outlook.com>
2023-08-06 15:48:48 +0900

    refactor: remove unnecessary include

--- ext/numo/tiny_linalg/tiny_linalg.hpp
+++ ext/numo/tiny_linalg/tiny_linalg.hpp
@@ -1,16 +1,10 @@
 #ifndef NUMO_TINY_LINALG_H
 #define NUMO_TINY_LINALG_H 1
 
-#if defined(TINYLINALG_USE_ACCELERATE)
-#include <Accelerate/Accelerate.h>
-#else
 #include <cblas.h>
 #include <lapacke.h>
-#endif
 
 #include <string>
-
-#include <cstring>
 
 #include <ruby.h>
 
