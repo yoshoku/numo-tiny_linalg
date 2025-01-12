521bf537b403d1c0940a14d457da2ac556c4bd91
yoshoku <yoshoku@outlook.com>
2025-01-13 08:50:47 +0900

    chore!: bump openblas to download from 0.3.28 to 0.3.29

--- ext/numo/tiny_linalg/extconf.rb
+++ ext/numo/tiny_linalg/extconf.rb
@@ -46,8 +46,8 @@ if build_openblas
 
   VENDOR_DIR = File.expand_path("#{__dir__}/../../../vendor")
   TINYLINALG_DIR = File.expand_path("#{__dir__}/../../../lib/numo/tiny_linalg")
-  OPENBLAS_VER = '0.3.28'
-  OPENBLAS_KEY = '0f54185b6ef804173c01b9a40520a0e8'
+  OPENBLAS_VER = '0.3.29'
+  OPENBLAS_KEY = '853a0c5c0747c5943e7ef4bbb793162d'
   OPENBLAS_URI = "https://github.com/OpenMathLib/OpenBLAS/archive/v#{OPENBLAS_VER}.tar.gz"
   OPENBLAS_TGZ = "#{VENDOR_DIR}/tmp/openblas.tgz"
 
